ee408dd06ae0ec9af0a4729d2918d6e1a3f4680b
mleroyy <mleroy@student.42.fr>
2016-11-24 21:04:42 +0100

    bim

--- get_next_line.h
+++ get_next_line.h
@@ -6,22 +6,23 @@
 /*   By: mleroy <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/11/20 16:09:15 by mleroy            #+#    #+#             */
-/*   Updated: 2016/11/23 19:52:31 by mleroy           ###   ########.fr       */
+/*   Updated: 2016/11/24 18:40:02 by mleroy           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
-# define BUFF_SIZE 10
+# define BUFF_SIZE 34
 
 #include "Libft/libft.h"
 
 typedef struct	s_info
 {
 	char	buf[BUFF_SIZE + 1];
-	char	*tmp;
-	char	*p;
-	int		line;
+	char	*tmp;//buffer temporaire pour le prochain appel de GNL
+	char	*tmp2;
+	char	*p;// pointeur sur le reste du buffer
+	int		line;//nombre de lignes lues
 }				t_info;
 int		get_next_line(const int fd, char **line);
 #endif
--- main.c
+++ main.c
@@ -1,13 +1,47 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   main.c                                             :+:      :+:    :+:   */
+/*   get_next_line.c                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: mleroy <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2016/11/20 16:31:44 by mleroy            #+#    #+#             */
-/*   Updated: 2016/11/20 16:31:48 by mleroy           ###   ########.fr       */
+/*   Created: 2016/11/20 16:07:58 by mleroy            #+#    #+#             */
+/*   Updated: 2016/11/24 20:58:40 by mleroy           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "get_next_line.h"
+#include <fcntl.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <stdio.h>
 
+void	ft_putnbr(int nb);
+void	ft_putchar(char c);
+
+int		main(int argc, char **argv)
+{
+	int		fd;
+	char	*line;
+	int		i;
+
+	i = 1;
+	if (argc < 2)
+	{
+		ft_putstr("Usage: 1 file minimum is required.\n");
+		return (1);
+	}
+	fd = open(argv[i], O_RDONLY);
+	if (fd == -1)
+	{
+		ft_putstr("Open failed.\n");
+		return (1);
+	}
+	//while((get_next_line(fd, &line) != 0))
+	//	ft_putchar(10);
+	get_next_line(fd, &line);
+		ft_putchar(10);
+	if (close(fd) == -1)
+		ft_putstr("Close failed.\n");
+	return (0);
+}
